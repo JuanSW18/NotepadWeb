b58d52de247f4d67a46cc8d7666dba4d544d3d59
Juan Eneque <juan.eneque@gestionysistemas.com>
2021-08-28 17:47:22 -0500

    script bash para automatizar instalación

--- RunMyNotepad.sh
+++ RunMyNotepad.sh
@@ -1,81 +1,41 @@
 #!/bin/bash
-# Este es nuestro primer programa
+# Ejecutar MyNotepad
 
-RUN_CONFIG=myNotepad.conf
-ENV_PATH=''
-PROJECT_PATH=''
+VIRTUALENV_NAME=envMyNotepad
+RUN_FILE=app.py
 
-# Colores
+RED='\033[0;31m'
 GREEN='\033[0;32m'
 CYAN='\033[0;36m'
 NC='\033[0m' # No Color
 
-# Funciones
-
-checkConfigFile () {
-if [ -f "$RUN_CONFIG" ]; then
-	file_exits=0
-else
-	file_exits=1
-fi
-return $file_exits
-}
-
-# -p: prompt -r: leer ruta
-makeConfigFile () {
-echo -e "${CYAN}** Configuracion inicial **${NC}"
-read -p 'Ruta del entorno virtual: ' -r envPath
-read -p 'Ruta de MyNotepad: ' -r projectPath
-
-ENV_PATH=$envPath
-PROJECT_PATH=$projectPath
-
-echo $envPath > $RUN_CONFIG
-echo $projectPath >> $RUN_CONFIG
-echo -e "${CYAN}Archivo de ejecución creado${NC}: ${GREEN}$RUN_CONFIG${NC}"
-echo ""
-}
-
-readConfigFile () {
-n=1
-while read -r line; do
-if [ $n -eq 1 ]; then
-	ENV_PATH=$line
-fi
-
-if [ $n -eq 2 ]; then
-	PROJECT_PATH=$line
-fi
-n=$((n+1))
-done < $RUN_CONFIG
-}
-
 activateEnv () {
-echo -e "${CYAN}Activando entorno virtual ...${NC}"
-cd "$ENV_PATH/Scripts"
-source activate
-echo -e "${GREEN}Entorno virtual activado${NC}"
-echo ""
+if [ -d $VIRTUALENV_NAME ]; then
+	echo -e "${CYAN}Activando entorno virtual ...${NC}"
+	cd "$VIRTUALENV_NAME/Scripts"
+	source activate
+	echo -e "${GREEN}Entorno virtual activado\n${NC}"
+	cd ..
+	cd ..
+else
+	echo -e "${RED} No existe entorno virtual: ${GREEN}$VIRTUALENV_NAME${NC}"
+fi
 }
 
 runNotepadProject () {
-echo -e "${CYAN}Iniciando MyNotepad ...${NC}"
-echo ""
-cd $PROJECT_PATH
-python app.py
+if [ -f $RUN_FILE ]; then
+	echo -e "${CYAN}Iniciando MyNotepad ...\n${NC}"
+	python $RUN_FILE
+else
+	echo -e "${RED} No existe archivo: ${GREEN}$RUN_FILE${NC}"
+fi
 }
 
-# Funcion principal
-mainFunction () {
-if checkConfigFile ; then
-	readConfigFile
-else
-	makeConfigFile
-fi
+main () {
 activateEnv
 runNotepadProject
 exit 0
 }
 
-mainFunction
+main
 $SHELL
